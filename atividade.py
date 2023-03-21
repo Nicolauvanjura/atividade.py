b1d1baf72cdee5cd344db870048edcc3d7dab8c3
unknown <vanjuranicolau@gmail.com>
2023-03-20 22:05:01 -0300

    dsfnlkdsnlf

---
+++ atividade.py
@@ -0,0 +1,13 @@
+nome = "Nicolau"
+sobrenome = "Vanjura"
+idade = 18
+altura = 1.66
+peso = 55.5
+maior_idade = idade >= 18
+
+# Exibindo resultados
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
